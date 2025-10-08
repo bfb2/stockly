4adcea21341597fcd29f47f78259cfac0e379857
brian <brianbaharloo@hotmail.com>
2025-10-08 18:15:14 -0500

    config redirect

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  redirects(){
+    return [
+      {
+        source:'/',
+        destination:'/backtrace-portfolio',
+        permanent:true
+      }
+    ]
+  }
 };
 
 export default nextConfig;
