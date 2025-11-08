df93bc263178abbb0725f21d94384c7631d429b1
brian <brianbaharloo@hotmail.com>
2025-11-08 03:25:24 -0600

    server side debug

--- frontend/src/app/backtrace-portfolio/page.tsx
+++ frontend/src/app/backtrace-portfolio/page.tsx
@@ -1,6 +1,7 @@
 import Backtrace from "./_components/Backtrace"
 
 const page = async () =>{
+    console.log('fetching data, init')
     const year = new Date().getFullYear()
     const fetchRes = await fetch(`https://stockly-fvoz.onrender.com/backtrace-portfolio?start_date=${year}-01-01&end_date=${year}-12-31&initial_amount=10000&rebalancing=No+Rebalancing&leverage=1&tickers=SPY&allocations=100,0,0&frequency=Annually&cashflows=None&contribution_amount=0&withdraw_amount=0&withdraw_pct=0&reinvest_dividends=true&expense_ratio=0`, {
             cache:'no-store',
